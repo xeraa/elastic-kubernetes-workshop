dfca6281d1e53c8a195d05cb66e53729d228e3e6
Philipp Krenn <pk@xeraa.net>
2021-05-04 03:02:16 +0200

    ADD port opening in Terraform
    And minor cleanup in confusing names

--- readme.md
+++ readme.md
@@ -81,7 +81,6 @@ Make sure you have run this before the demo.
 1. Have your AWS account set up, access key created, and added as environment variables in `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`. Protip: Use [https://github.com/sorah/envchain](https://github.com/sorah/envchain) to keep your environment variables safe.
 1. Change the settings to a domain you have registered under Route53 in *inventory* and *variables.tf*. Set the Hosted Zone for that domain and export the Zone ID under the environment variable `TF_VAR_zone_id`. If you haven't created the Hosted Zone yet, you should set it up in the AWS Console first and then set the environment variable.
 1. If you haven't installed the AWS plugin for Terraform, get it with `terraform init` first. Then create the keypair, DNS settings, and instances with `terraform apply`.
-1. Open and TCP/5601 and TCP/8080 on the network configuration (waiting for this [Terraform issue](https://github.com/terraform-providers/terraform-provider-aws/issues/700)).
 1. Apply the configuration to the instance with `ansible-playbook configure.yml`.
 
 When you are done, remove the instances, DNS settings, and key with `terraform destroy`.
--- terraform.tf
+++ terraform.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 
 # Create the SSH key pair
-resource "aws_lightsail_key_pair" "auditd_key_pair" {
-  name       = "auditd_key_pair"
+resource "aws_lightsail_key_pair" "aws_key_pair" {
+  name       = "aws_key_pair"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
@@ -17,8 +17,35 @@ resource "aws_lightsail_instance" "instance" {
   availability_zone = "${var.region}a"
   blueprint_id      = var.operating_system
   bundle_id         = var.size
-  key_pair_name     = "auditd_key_pair"
-  depends_on        = [aws_lightsail_key_pair.auditd_key_pair]
+  key_pair_name     = "aws_key_pair"
+  depends_on        = [aws_lightsail_key_pair.aws_key_pair]
+}
+resource "aws_lightsail_instance_public_ports" "instance" {
+  instance_name = aws_lightsail_instance.instance.name
+  # SSH (defaults are overwritten so this must be specified)
+  port_info {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+  }
+  # So Let's Encrypt can generate its certificate
+  port_info {
+    protocol  = "tcp"
+    from_port = 80
+    to_port   = 80
+  }
+  # Kibana
+  port_info {
+    protocol  = "tcp"
+    from_port = 5601
+    to_port   = 5601
+  }
+  # Alternative HTTP
+  port_info {
+    protocol  = "tcp"
+    from_port = 8080
+    to_port   = 8080
+  }
 }
 resource "aws_route53_record" "apex" {
   zone_id = var.zone_id
